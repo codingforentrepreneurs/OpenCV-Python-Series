98933aef84873b8bc68500ef88417a367733c933
Justin Mitchel <jmitchel3@gmail.com>
2018-04-17 15:24:19 -0700

    How to create  a timelapse with OpenCV and Python

--- src/lessons/timelapse-how-to.py
+++ src/lessons/timelapse-how-to.py
@@ -1,57 +1,60 @@
+# joincfe.com/github/
+# lessons/
+# https://www.youtube.com/playlist?list=PLEsfXFp6DpzRyxnU-vfs3vk-61Wpt7bOS
+import datetime
+import time
 import os
 import numpy as np
 import cv2
-import time
-import datetime
-
-from utils import CFEVideoConf, image_resize
 import glob
 
+from utils import CFEVideoConf, image_resize
+
 cap = cv2.VideoCapture(0)
-
-
-frames_per_seconds = 20
-save_path='saved-media/timelapse.mp4'
+save_path = 'saved-media/timelapse.mp4'
+frames_per_seconds = 24.0
 config = CFEVideoConf(cap, filepath=save_path, res='720p')
 out = cv2.VideoWriter(save_path, config.video_type, frames_per_seconds, config.dims)
-timelapse_img_dir = 'images/timelapse/'
-seconds_duration = 20
+seconds_duration = 50
 seconds_between_shots = .25
+timelapse_img_dir = "images/timelapse/"
 
 if not os.path.exists(timelapse_img_dir):
     os.mkdir(timelapse_img_dir)
 
+
 now = datetime.datetime.now()
 finish_time = now + datetime.timedelta(seconds=seconds_duration)
+
 i = 0
+
 while datetime.datetime.now() < finish_time:
-    '''
-    Ensure that the current time is still less
-    than the preset finish time
-    '''
-    ret, frame      = cap.read()
-    filename        = f"{timelapse_img_dir}/{i}.jpg"
-    i               += 1
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    filename = f"{timelapse_img_dir}/{i}.jpg"
+    i += 1
     cv2.imwrite(filename, frame)
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # out.write(frame)
+    # Display the resulting frame
+    # cv2.imshow('frame',frame)
     time.sleep(seconds_between_shots)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
+clear_images = True
 
-def images_to_video(out, image_dir, clear_images=True):
-    image_list = glob.glob(f"{image_dir}/*.jpg")
+def images_to_video(out, img_dir, clear_images=True):
+    image_list = glob.glob(f"{img_dir}/*.jpg")
     sorted_images = sorted(image_list, key=os.path.getmtime)
     for file in sorted_images:
-        image_frame  = cv2.imread(file)
+        image_frame = cv2.imread(file)
         out.write(image_frame)
     if clear_images:
-        '''
-        Remove stored timelapse images
-        '''
         for file in image_list:
             os.remove(file)
 
-images_to_video(out, timelapse_img_dir)
+images_to_video(out, timelapse_img_dir, clear_images=False)
 # When everything done, release the capture
 cap.release()
 out.release()
