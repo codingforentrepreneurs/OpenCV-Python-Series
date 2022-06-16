c34f000f0c253139f19effa6d4bc50a08f989367
Panasak <89185433+Panasak@users.noreply.github.com>
2022-06-16 10:37:50 +0200

    commit

--- src/faces-train.py
+++ src/faces-train.py
@@ -7,8 +7,9 @@ import pickle
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
+
 
 current_id = 0
 label_ids = {}
